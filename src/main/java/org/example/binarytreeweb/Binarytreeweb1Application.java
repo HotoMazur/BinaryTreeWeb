4bb4aa1b770defae17a947d805f78b3fa89a109b
Dmytro Mazurev <dmytro.mazurev@icloud.com>
2025-04-10 15:30:19 +0300

    first commit

---
+++ src/main/java/org/example/binarytreeweb/Binarytreeweb1Application.java
@@ -0,0 +1,13 @@
+package org.example.binarytreeweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Binarytreeweb1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Binarytreeweb1Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/binarytreeweb/Binarytreeweb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.binarytreeweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Binarytreeweb1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
